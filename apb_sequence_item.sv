119a7181134a921830077fd5884f086fde53550d
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-21 16:36:59 +0530

    Update apb_sequence_item.sv

--- apb_sequence_item.sv
+++ apb_sequence_item.sv
@@ -1,10 +1,10 @@
 class apb_sequence_item extends uvm_sequence_item;
-  rand logic transfer;
-  rand logic [7:0]apb_write_paddr;
-  rand logic [7:0]apb_write_data;
-  rand logic [7:0]apb_read_paddr;
-  rand logic [7:0]apb_read_data_out;
-  logic READ_WRITE;
+  rand bit transfer;
+  rand logic [`AW-1:0]apb_write_paddr;
+  rand logic [`DW-1:0]apb_write_data;
+  rand logic [`AW-1:0]apb_read_paddr;
+  rand logic [`DW-1:0]apb_read_data_out;
+  rand bit READ_WRITE;
   
   `uvm_object_utils_begin(apb_sequence_item)
   `uvm_field_int(transfer, UVM_ALL_ON)
