62fc95c7a226531bb51290a3289a35cd12b83cb2
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-21 16:44:36 +0530

    Update apb_driver.sv

--- apb_driver.sv
+++ apb_driver.sv
@@ -10,7 +10,6 @@ class apb_driver extends uvm_driver;
   
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    req = apb_sequence_item::type_id::create("req", this);
     if(!uvm_config_db #(virtual apb_if)::get(this, "*", "vif", vif))
       `uvm_fatal(get_type_name(), "cant get virtual interface");
   endfunction
@@ -18,6 +17,7 @@ class apb_driver extends uvm_driver;
   task run_phase(uvm_phase phase);
     super.run_phase(phase);
     forever begin
+      wait(vif.presetn);
       seq_item_port.get_next_item(req);
       drive();
       seq_item_port.item_done();
@@ -25,15 +25,6 @@ class apb_driver extends uvm_driver;
   endtask
   
   virtual task drive();
-    @(vif.drv_cb)
-    begin
-    vif.drv_cb.transfer <= req.transfer;
-    vif.drv_cb.pwrite <= req.pwrite;
-    vif.drv_cb.pwaddr <= req.pwaddr;
-    vif.drv_cb.i_pwdata <= req.pwdata;
-    vif.drv_cb.praddr <= req.praddr;
-    `uvm_info("DRIVER OUTPUT","INSIDE DRIVER",UVM_LOW);
-    req.print();
-    end
+  
   endtask
 endclass
