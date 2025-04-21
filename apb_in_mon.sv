673b8967ad226ad94a56b5524b0daa5425c43623
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-21 14:15:22 +0530

    Create apb_in_mon.sv

---
+++ apb_in_mon.sv
@@ -0,0 +1,21 @@
+class apb_in_mon extends uvm_monitor;
+  `uvm_component_utils(apb_in_mon)
+  virtual apb_if vif;
+  uvm_analysis_port#(apb_sequence_item) mon_in;
+  apb_sequence_item item;
+  
+  function new(string name = "apb_in_mon", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    mon_in = new("mon_in",this);
+    item_collect_port = new("item_collect_port", this);
+    if(!uvm_config_db#(virtual apb_if)::get(this,"*","vif",vif))
+      `uvm_fatal(get_type_name(),"cant get virtual interface");
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+  endtask
+endclass
