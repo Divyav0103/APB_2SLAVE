002bc753786262ccd2b336d8291c4be99e38531f
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-21 16:52:17 +0530

    Update apb_top.sv

--- apb_top.sv
+++ apb_top.sv
@@ -14,18 +14,21 @@ module top();
   //DUT instantiation
   apb_slave#(.AW(`AW),.DW(`DW))
   dut(
+    .pclk(inf.pclk),
+    .presetn(inf.presetn),
     .transfer(inf.trasnfer),
     .READ_WRITE(inf.READ_WRITE),
     .apb_write_paddr(inf.apb_write_paddr),
     .apb_write_data(inf.apb_write_data),
     .apb_read_paddr(inf.apb_read_paddr)
-    .apb_read_);
+    .apb_read_data_out(inf.apb_read_data_out)
+  );
   
   initial begin
-    uvm_config_db#(virtual apb_if)::set(null,"*","vif", inf);
-    
     $dumpfile("dump.vcd");
-    $dumpvars;
+    $dumpvars();
+    pclk = 0;
+    forever #5 pclk = ~pclk
   end
   
   initial begin
