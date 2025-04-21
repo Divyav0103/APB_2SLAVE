74beffc3cece8dec25ee26f0434a551263b0be5d
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-21 16:33:03 +0530

    Update apb_interface.sv

--- apb_interface.sv
+++ apb_interface.sv
@@ -1,29 +1,24 @@
 `include "apb_define.svh"
 interface apb_if(input bit pclk,input bit presetn);
   logic transfer;
-  logic [7:0]apb_write_paddr;
-  logic [7:0]apb_write_data;
-  logic [7:0]apb_read_paddr;
-  logic [7:0]apb_read_data_out;
+  logic [`AW-1:0]apb_write_paddr;
+  logic [`DW-1:0]apb_write_data;
+  logic [`AW-1:0]apb_read_paddr;
+  logic [`DW-1:0]apb_read_data_out;
   logic READ_WRITE;
   
-  clocking drv_cb@(posedge pclk);
-    default input#0 output#0;
-    output transfer;
-    output presetn;
-    output [7:0]apb_write_paddr;
-    output [7:0]apb_write_data;
-    output [7:0]apb_read_paddr;
-    output [7:0]apb_read_data_out;
-    output READ_WRITE;
+  clocking drv_cb@(posedge pclk or negedge presetn);
+    default input #0 output #0;
+    output transfer, READ_WRITE, apb_write_paddr, apb_write_data, apb_read_paddr;
+    input presetn;
   endclocking
   
-  clocking apb_mon@(posedge pclk);
-    default input#0 output#0;
-    input [7:0] apb_read_data_out;
+  clocking amon_cb@(posedge pclk) or negedge presetn);
+    default input #0 output #0;
+    input apb_read_data_out;
   endclocking
     
   
   modport DRV(clocking drv_cb);
-    modport APB_MON(clocking apb_mon);
+  modport MON(clocking apb_mon);
 endinterface
