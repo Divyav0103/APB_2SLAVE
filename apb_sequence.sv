e211e6a32b38d1abdda2ad6627f3fdba86ed1c49
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-21 16:34:14 +0530

    Update apb_sequence.sv

--- apb_sequence.sv
+++ apb_sequence.sv
@@ -1,13 +1,12 @@
-
 class apbsequence extends uvm_sequence;
-  `uvm_object_utils(APB_2sequence)
+  `uvm_object_utils(apb_sequence)
   
   function new(string name = "apb_sequence");
     super.new(name);
   endfunction
   
   virtual task body();
-    req = apbsequence_item::type_id::create("req");
+    req = apb_sequence_item::type_id::create("req");
     wait_for_grant();
     req.randomize();
     send_request(req);
