525637ea5bda1831737f5da761fbf9700418336a
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-22 14:00:36 +0530

    Update apb_sequencer.sv

--- apb_sequencer.sv
+++ apb_sequencer.sv
@@ -1,3 +1,13 @@
+/------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------
+// Project      : APB
+// File Name    : apb_sequencer.sv
+// Developer    : Divya V 
+//------------------------------------------------------------------------------
+// Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
+//------------------------------------------------------------------------------
+
 class apb_sequencer extends uvm_sequencer;
   `uvm_component_utils(apb_sequencer)
   function new(string name = "apb_sequencer",uvm_component parent);
