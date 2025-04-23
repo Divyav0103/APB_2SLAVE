d91b999c1898374b1e4f27fd1109974507e8f63d
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-23 10:20:29 +0530

    Update apb_passive_agent.sv

--- apb_passive_agent.sv
+++ apb_passive_agent.sv
@@ -7,7 +7,9 @@
 //------------------------------------------------------------------------------
 
 class apb_passive_agent extends uvm_agent;
+  
   `uvm_component_utils(apb_passive_agent)
+  
   apb_out_monitor output_mon;
   
   function new(string name = "apb_passive_agent", uvm_component parent);
@@ -18,4 +20,5 @@ class apb_passive_agent extends uvm_agent;
     super.build_phase(phase);
     output_mon = apb_out_monitor::type_id::create("output_mon", this);
   endfunction: build_phase
-endclass: apb_passive_agent
+
+endclass
