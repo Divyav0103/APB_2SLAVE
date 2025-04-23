9008013f392fe7b114c80c88cc87ff47b4303267
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-23 10:41:36 +0530

    Update apb_in_mon.sv

--- apb_in_mon.sv
+++ apb_in_mon.sv
@@ -7,10 +7,13 @@
 //------------------------------------------------------------------------------
 
 class apb_in_mon extends uvm_monitor;
+  
   `uvm_component_utils(apb_in_mon)
   
   virtual apb_if vif;
-  uvm_analysis_port#(apb_sequence_item) mon_in;
+  
+  uvm_analysis_port#(apb_sequence_item) mon_in2sb;
+  
   apb_sequence_item item;
   
   function new(string name = "apb_in_mon", uvm_component parent);
@@ -20,11 +23,24 @@ class apb_in_mon extends uvm_monitor;
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     mon_in = new("mon_in",this);
-    item_collect_port = new("item_collect_port", this);
     if(!uvm_config_db#(virtual apb_if)::get(this,"*","vif",vif))
-      `uvm_fatal(get_type_name(),"cant get virtual interface");
+      `uvm_fatal("Input Monitor","cant get virtual interface");
   endfunction
   
   task run_phase(uvm_phase phase);
+    repeat(2) @(vif.mon_cb);
+    forever begin
+      item = apb_sequence_item::type_id::create("item", this);
+      @(vif.mon_cb)
+      begin
+        item.i_ptransfer = vif.mon_cb.i_ptransfer;
+        item.i_pwrite = vif.mon_cb.i_pwrite;
+	    item.i_pwaddr = vif.mon_cb.i_pwaddr;
+		item.i_pwdata = vif.mon_cb.i_pwdata;
+		item.i_praddr = vif.mon_cb.i_praddr;
+        `uvm_info("input mon", $sformatf("---Input mon---"), UVM_LOW);
+        item.print();
+        `uvm_info("input mon", $sformatf("-------------------"), UVM_LOW);	
+        mon_in2sb.write(item);
   endtask
 endclass
