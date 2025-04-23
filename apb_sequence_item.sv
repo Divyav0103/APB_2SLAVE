a3fd7eda9fc2dc42dd0b9e5ed5765f6970f65049
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-23 09:59:09 +0530

    Update apb_sequence_item.sv

--- apb_sequence_item.sv
+++ apb_sequence_item.sv
@@ -7,20 +7,20 @@
 //------------------------------------------------------------------------------
 
 class apb_sequence_item extends uvm_sequence_item;
-  rand bit transfer;
-  rand logic [`AW-1:0]apb_write_paddr;
-  rand logic [`DW-1:0]apb_write_data;
-  rand logic [`AW-1:0]apb_read_paddr;
-  rand logic [`DW-1:0]apb_read_data_out;
-  rand bit READ_WRITE;
+  rand bit i_ptransfer;
+  rand logic [`AW-1:0]i_pwaddr;
+  rand logic [`DW-1:0]i_pwdata;
+  rand logic [`AW-1:0]i_praddr;
+  rand logic [`DW-1:0]o_prdata;
+  rand bit i_pwrite;
   
   `uvm_object_utils_begin(apb_sequence_item)
-  `uvm_field_int(transfer, UVM_ALL_ON)
-  `uvm_field_int(apb_write_paddr, UVM_ALL_ON)
-  `uvm_field_int(apb_write_data, UVM_ALL_ON)
-  `uvm_field_int(apb_read_paddr, UVM_ALL_ON)
-  `uvm_field_int(apb_read_data_out, UVM_ALL_ON)
-  `uvm_field_int(READ_WRITE, UVM_ALL_ON)
+  `uvm_field_int(i_ptransfer, UVM_ALL_ON)
+  `uvm_field_int(i_pwaddr, UVM_ALL_ON)
+  `uvm_field_int(i_pwdata, UVM_ALL_ON)
+  `uvm_field_int(i_praddr, UVM_ALL_ON)
+  `uvm_field_int(i_pwrite, UVM_ALL_ON)
+  `uvm_field_int(o_prdata, UVM_ALL_ON)
   `uvm_object_utils_end
   
   function new(string name = "apb_sequence_item");
@@ -28,4 +28,3 @@ class apb_sequence_item extends uvm_sequence_item;
   endfunction: new
   
 endclass
-   
