7682801b9976160859eabbbfe361069b908d034e
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-23 10:23:16 +0530

    Update apb_driver.sv

--- apb_driver.sv
+++ apb_driver.sv
@@ -10,7 +10,6 @@ class apb_driver extends uvm_driver#(apb_sequence_item);
   `uvm_component_utils(apb_driver)
   
   virtual apb_if vif;
-  uvm_analysis_port #(apb_sequence_item) item_collect_port;
   
   function new(string name = "apb_driver", uvm_component parent);
     super.new(name, parent);
