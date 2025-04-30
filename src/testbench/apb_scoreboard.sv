4f86f515a6fb18eced78550f7378c83d3c716d0b
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-04-30 13:34:59 +0530

    Update apb_scoreboard.sv

--- src/testbench/apb_scoreboard.sv
+++ src/testbench/apb_scoreboard.sv
@@ -12,8 +12,6 @@ class apb_scoreboard extends uvm_scoreboard;
  
   `uvm_component_utils(apb_scoreboard)
   
- logic [`DW - 1:0] mem[0:255];
-  
   virtual apb_if vif;
   
   uvm_analysis_imp_ip#(apb_sequence_item, apb_scoreboard)aport_ip;
@@ -37,60 +35,88 @@ class apb_scoreboard extends uvm_scoreboard;
     aport_ip = new("aport_ip", this);
     aport_op = new("aport_op", this);
     if(!uvm_config_db#(virtual apb_if)::get(this, "*", "vif",vif))
-       `uvm_fatal("Scoreboard", "Unable to get the virtual interface")*/
+       `uvm_fatal("Scoreboard", "Unable to get the virtual interface")
   endfunction
 
     
      virtual function void write_ip(apb_sequence_item i_tr);
       exp_q.push_back(i_tr);
-      `uvm_info("Input Queue",$sformatf("Expected tx: queue size = %d, transfer = %b, apb_write_paddr = %0h, apb_write_data = %b, apb_read_paddr = %0h, read_write = %b, apb_read_data_out = %0h", exp_q.size(), i_tr.transfer, i_tr.apb_write_paddr, i_tr.apb_write_data, i_tr.apb_read_paddr, i_tr.read_write, i_tr.apb_read_data_out), UVM_LOW);
       $display("------------------------------------------------------------------------------------");
      endfunction
 
        virtual function void write_op(apb_sequence_item o_tr);
         act_q.push_back(o_tr);
-        `uvm_info("Output Queue",$sformatf("Actual tx: queue size = %d, transfer = %b, apb_write_paddr = %0h, apb_write_data = %b, apb_read_paddr = %0h, read_write = %b, apb_read_data_out = %0h", act_q.size(), o_tr.transfer, o_tr.apb_write_paddr, o_tr.apb_write_data, o_tr.apb_read_paddr, o_tr.read_write, o_tr.apb_read_data_out), UVM_LOW);
          $display("------------------------------------------------------------------------------------");
       endfunction
     
          
        function void value_display(apb_sequence_item exp_pkt, apb_sequence_item act_pkt);
-        if(exp_pkt.READ_WRITE) begin
-         if((exp_pkt.apb_write_data == act_pkt.apb_write_data) && (exp_pkt.apb_read_data_out == act_pkt.apb_read_data_out))
-           begin
-            `uvm_info("MATCH","",UVM_LOW)
-           end
-         else begin
-          `uvm_info("MISMATCH","",UVM_LOW)
-         end
-       endfunction
+         `uvm_info("Check start","----------------------------------------------------Start----------------------------------------------",UVM_LOW);
+
+         `uvm_info("expected","Expected Packet Values:", UVM_LOW);
+         exp_pkt.print();
+         
+         `uvm_info("Preset check", $sformatf("-----------PRST = %d---------------",vif.presetn), UVM_LOW);
+         
+         `uvm_info("actual", "Actual Packet Values:", UVM_LOW);
+         act_pkt.print();
+         `uvm_info("VALUE_MATCH", $sformatf("-----------match = %0d, mismatch = %0d-----------",match, mismatch), UVM_LOW);
+         
+         if (mismatch) begin
+
+	  `uvm_error("MISMATCH", $sformatf("-----------Sequence Mismatched-----------"));
+
+	   mismatch = 0;
+
+  end
+
+  if (match) begin
+
+	  `uvm_info("MATCH", $sformatf("-----------Sequence matched-----------"), UVM_LOW);
+
+            match = 0;
+
+  end
+ `uvm_info("Check_stop", "-----------------------------Stop Check-----------------------", UVM_LOW);
+
+     endfunction
        
        task run_phase(uvm_phase phase);
-       `uvm_info("sb","Entering uvm_scoreboard",UVM_LOW);
+         apb_sequence_item in;
+         apb_sequence_item out;
+         
+        bit [7:0] mem[0:255];
+      
         super.run_phase(phase);
         forever begin
+          
+            `uvm_info("scb", $sformatf("###################### inside scb ##########################"), UVM_LOW)
+
+    `uvm_info("scb", $sformatf("##############EXPECTED_OP_SIZE=%0d#####ACTUAL_OP_SIZE=%0D####################",exp_q.size, act_q.size), UVM_LOW)
+
         exp_pkt = apb_sequence_item::type_id::create("exp_pkt", this);
         act_pkt = apb_sequence_item::type_id::create("act_pkt", this);
         
            wait(exp_q.size() > 0 && act_q.size() > 0) begin
-             exp_pkt = exp_q.pop_front();
-             act_pkt = act_q.pop_front();
              
-            if(exp_pkt.transfer == 1) begin
-             if(exp_pkt.read_write == 1) begin
-                 mem[exp_pkt.apb_write_paddr] = exp_pkt.apb_write_data;
-                 //value_display(exp_pkt, act_pkt);
-               end
-               else begin
-                 exp_pkt.apb_read_data_out = mem[exp_pkt.apb_read_paddr];
-                 //value_display(exp_pkt, act_pkt);
-               end
-             end
-            value_display(exp_pkt, act_pkt);
+             in = exp_q.pop_front();
+             out = act_q.pop_front();
            end
-         end
+          if(!vif.presetn)) begin
+            in.transfer = 0;
+            out.read_write = 0;
+            value_display(in, out);
+          end
+          
+          else begin
+            if(in.read_write) begin
+              mem[in.apb_write_paddr] = in.apb_write_data;
+            end
+            else begin
+              in.apb_read_paddr = in.apb_write_paddr;
+              in.apb_read_data_out = mem[in.apb_read_paddr];
+            end
+          end
+        end
        endtask
-  endclass
-         
-         
-         
+endclass
