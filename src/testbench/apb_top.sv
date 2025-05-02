2c3fca338b7a7e8f768ebcd2b6482234d8ad8b35
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-05-02 21:16:43 +0530

    Update apb_top.sv

--- src/testbench/apb_top.sv
+++ src/testbench/apb_top.sv
@@ -5,21 +5,22 @@
 //------------------------------------------------------------------------------
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
-import uvm_pkg::*;
+//import uvm_pkg::*;
 
-`include "uvm_macros.svh"
+//`include "uvm_macros.svh"
 `include "apb_define.sv"
 `include "apb_package.sv"
 `include "apbtop.v"
 
 module top();
   
-  logic presetn;
-  logic pclk;
+  bit presetn;
+  bit pclk;
   
   initial begin
-    presetn = 0;
-    #10 presetn = 1;
+    //presetn = 0;
+    //#10 
+    presetn = 1;
   end
   
   apb_if inf(.pclk(pclk),.presetn(presetn));
@@ -39,12 +40,15 @@ module top();
   end
 
   initial begin
-   uvm_config_db#(virtual apb_if.DRV)::set(null, "*", "vif",inf);
-   uvm_config_db#(virtual apb_if.MON)::set(null, "*", "vif", inf);
+   uvm_config_db#(virtual apb_if)::set(null, "*", "vif",inf);
+   //uvm_config_db#(virtual apb_if.MON)::set(null, "*", "vif", inf);
+
+    $dumpfile("dump.vcd");
+    $dumpvars();
   end
   
   initial begin
-   run_test("apb_test");
+    run_test("apb_write_s0");
   end
 endmodule
 
