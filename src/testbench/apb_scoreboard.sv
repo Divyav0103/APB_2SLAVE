4bf1c1f07fb93d656a262c187b98a2be2dc30ca9
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-05-02 21:48:03 +0530

    Update apb_scoreboard.sv

--- src/testbench/apb_scoreboard.sv
+++ src/testbench/apb_scoreboard.sv
@@ -12,12 +12,12 @@ class apb_scoreboard extends uvm_scoreboard;
 
   `uvm_component_utils(apb_scoreboard)
 
-  bit [7:0] mem1[0:255];
-  bit [7:0] mem2[0:255];
+  logic [`DW-1:0] mem[511:0];
+  //bit [7:0] mem2[0:255];
 
   // Changed class name to match the declared class
-  uvm_analysis_imp_ip#(apb_seq_item, apb_scoreboard) aport_ip;
-  uvm_analysis_imp_op#(apb_seq_item, apb_scoreboard) aport_op;
+  uvm_analysis_imp_ip #(apb_seq_item, apb_scoreboard) aport_ip;
+  uvm_analysis_imp_op #(apb_seq_item, apb_scoreboard) aport_op;
 
   virtual apb_if vif;
 
@@ -26,6 +26,9 @@ class apb_scoreboard extends uvm_scoreboard;
 
   apb_seq_item exp_q[$];
   apb_seq_item act_q[$];
+
+  int mat = 0;
+  int mis = 0;
 
   function new(string name = "apb_scoreboard", uvm_component parent);
     super.new(name, parent);
@@ -42,50 +45,61 @@ class apb_scoreboard extends uvm_scoreboard;
   virtual function void write_ip(apb_seq_item in_txn);
     exp_q.push_back(in_txn);
     $display("### Expected transaction pushed to queue ###");
+    $display("---------------------------------------------------------------------------------------");
   endfunction
 
   virtual function void write_op(apb_seq_item out_txn);
     act_q.push_back(out_txn);
     $display("### Actual transaction pushed to queue ###");
+    $display("---------------------------------------------------------------------------------------");
   endfunction
 
-  function void compare(apb_seq_item exp_pkt, apb_seq_item act_pkt);
-    if (exp_pkt.apb_write_data == act_pkt.apb_write_data &&
-        exp_pkt.apb_read_data_out == act_pkt.apb_read_data_out) begin
-      exp_pkt.print();
-      `uvm_info("MATCH", "Expected and actual packets matched", UVM_LOW)
-      act_pkt.print();
+  virtual function void compare(apb_seq_item exp_pkt, apb_seq_item act_pkt);
+    if(exp_pkt.read_write) begin
+      if (exp_pkt.apb_write_data == act_pkt.apb_write_data) && (exp_pkt.apb_write_paddr == act_pkt.apb_rwrite_paddr)) begin
+      //exp_pkt.print();
+        `uvm_info("MATCH",$sformatf( "----------------------MATCH------------------------\n Expected apb_write_data = %0h |  Actual apb_write_data = %0h |  Expected apb_write_paddr =%0h |  Actual apb_write_apddr = %0h ", exp_pkt.apb_write_data, act_pkt.apb_write_data, exp_pkt.apb_write_paddr, act_pkt.apb_write_paddr), UVM_LOW);
+      //act_pkt.print();
+        mat++;
     end else begin
-      exp_pkt.print();
-      act_pkt.print();
-      `uvm_error("MISMATCH", "Expected and actual packets do not match");
+      `uvm_info("MISMATCH",$sformatf( "----------------------MISMATCH------------------------\n Expected apb_write_data = %0h |  Actual apb_write_data = %0h |  Expected apb_write_paddr =%0h |  Actual apb_write_apddr = %0h ", exp_pkt.apb_write_data, act_pkt.apb_write_data, exp_pkt.apb_write_paddr, act_pkt.apb_write_paddr), UVM_LOW);
+       mis++;
+    end
+    end
+
+    else begin
+      if ((exp_pkt.apb_read_data_Out == act_pkt.apb_read_data_Out) && (exp_pkt.apb_read_paddr == act_pkt.apb_read_paddr)) begin 
+        `uvm_info("MATCH",$sformatf( "----------------------MATCH------------------------\n Expected apb_write_data = %0h |  Actual apb_write_data = %0h |  Expected apb_write_paddr =%0h |  Actual apb_write_apddr = %0h ", exp_pkt.apb_write_data, act_pkt.apb_write_data, exp_pkt.apb_write_paddr, act_pkt.apb_write_paddr), UVM_LOW);
+      //act_pkt.print();
+        mat++;
+    end else begin
+      `uvm_info("MISMATCH",$sformatf( "----------------------MISMATCH------------------------\n Expected apb_write_data = %0h |  Actual apb_write_data = %0h |  Expected apb_write_paddr =%0h |  Actual apb_write_apddr = %0h ", exp_pkt.apb_write_data, act_pkt.apb_write_data, exp_pkt.apb_write_paddr, act_pkt.apb_write_paddr), UVM_LOW);
+       mis++;
+    end
     end
   endfunction
 
   task run_phase(uvm_phase phase);
+    super.run_phase(phase);
     forever begin
+      exp_pkt = apb_sequence_item::type_id::create("exp_pkt",this);
+      act_pkt = apb_sequence_item::type_id::create("act_pkt",this);
+     
       wait (exp_q.size() > 0 && act_q.size() > 0);
       
       exp_pkt = exp_q.pop_front();
       act_pkt = act_q.pop_front();
 
-      if (exp_pkt.transfer) begin
-        if (exp_pkt.read_write) begin
-          // Write operation
-          case (exp_pkt.apb_write_addr[7])
-            1'b0: mem1[exp_pkt.apb_write_addr] = exp_pkt.apb_write_data;
-            1'b1: mem2[exp_pkt.apb_write_addr] = exp_pkt.apb_write_data;
-          endcase
-          compare(exp_pkt, act_pkt);
-        end else begin
-          // Read operation
-          case (exp_pkt.apb_read_addr[7])
-            1'b0: exp_pkt.apb_read_data_out = mem1[exp_pkt.apb_read_addr];
-            1'b1: exp_pkt.apb_read_data_out = mem2[exp_pkt.apb_read_addr];
-          endcase
-          compare(exp_pkt, act_pkt);
-        end
+      if (exp_pkt.transfer == 1) begin
+        if (exp_pkt.read_write == 0) begin
+          mem[exp_pkt.apb_write_paddr] = exp_pkt.apb_write_data;
+          end else begin
+            exp_pkt.apb_read_out = mem[exp_pkt.apb_read_paddr];
+          end
       end
+          compare(exp_pkt, act_pkt);
+        //end
+      //end
     end
   endtask
 
