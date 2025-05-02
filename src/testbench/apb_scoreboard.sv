e0044c757266ad294b9c34c05e118d9b3f3538b8
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-05-02 09:54:24 +0530

    updated

--- src/testbench/apb_scoreboard.sv
+++ src/testbench/apb_scoreboard.sv
@@ -23,8 +23,8 @@ class apb_scoreboard extends uvm_scoreboard;
   apb_sequence_item exp_q[$];
   apb_sequence_item act_q[$];
 
-  int pass = 0;
-  int fail = 0;
+  int match;
+  int mismatch;
   
   function new(string name = "apb_scoreboard", uvm_component parent);
     super.new(name, parent);
@@ -59,61 +59,62 @@ class apb_scoreboard extends uvm_scoreboard;
          `uvm_info("Preset check", $sformatf("-----------PRST = %d---------------",vif.presetn), UVM_LOW);
          
          `uvm_info("actual", "Actual Packet Values:", UVM_LOW);
-         act_pkt.print();
+          act_pkt.print();
+
          `uvm_info("VALUE_MATCH", $sformatf("-----------match = %0d, mismatch = %0d-----------",match, mismatch), UVM_LOW);
          
-         if (mismatch) begin
+         if(mismatch) begin
 
 	  `uvm_error("MISMATCH", $sformatf("-----------Sequence Mismatched-----------"));
 
 	   mismatch = 0;
 
-  end
+         end
 
-  if (match) begin
+         if(match) begin
 
 	  `uvm_info("MATCH", $sformatf("-----------Sequence matched-----------"), UVM_LOW);
+           match = 0;
 
-            match = 0;
+         end
+          `uvm_info("Check_stop", "-----------------------------Stop Check-----------------------", UVM_LOW);
 
-  end
- `uvm_info("Check_stop", "-----------------------------Stop Check-----------------------", UVM_LOW);
-
-     endfunction
+       endfunction
        
        task run_phase(uvm_phase phase);
          apb_sequence_item in;
          apb_sequence_item out;
-         
-        bit [7:0] mem[0:255];
+         bit [7:0] mem[0:255];
       
-        super.run_phase(phase);
+        //super.run_phase(phase);
         forever begin
-          
-            `uvm_info("scb", $sformatf("###################### inside scb ##########################"), UVM_LOW)
+ 	repeat(3)@(posedge vif.mon_cb);
+           
+	    `uvm_info("scb", $sformatf("###################### inside scb ##########################"), UVM_LOW)
 
-    `uvm_info("scb", $sformatf("##############EXPECTED_OP_SIZE=%0d#####ACTUAL_OP_SIZE=%0D####################",exp_q.size, act_q.size), UVM_LOW)
+            `uvm_info("scb", $sformatf("##############EXPECTED_OP_SIZE=%0d#####ACTUAL_OP_SIZE=%0d####################",exp_q.size, act_q.size), UVM_LOW)
 
-        exp_pkt = apb_sequence_item::type_id::create("exp_pkt", this);
-        act_pkt = apb_sequence_item::type_id::create("act_pkt", this);
+            exp_pkt = apb_sequence_item::type_id::create("exp_pkt", this);
+            act_pkt = apb_sequence_item::type_id::create("act_pkt", this);
         
-           wait(exp_q.size() > 0 && act_q.size() > 0) begin
-             
-             in = exp_q.pop_front();
-             out = act_q.pop_front();
-           end
-          if(!vif.presetn)) begin
+            wait(exp_q.size() > 0 && act_q.size() > 0);
+		begin
+            		in = exp_q.pop_front();
+            		out = act_q.pop_front();
+		end
+         
+        if(!vif.presetn) begin
             in.transfer = 0;
             out.read_write = 0;
             value_display(in, out);
-          end
+        end
           
-          else begin
+         else begin
             if(in.read_write) begin
               mem[in.apb_write_paddr] = in.apb_write_data;
             end
-            else begin
-              in.apb_read_paddr = in.apb_write_paddr;
+         else begin
+            // if(in.apb_read_paddr == in.apb_write_paddr)
               in.apb_read_data_out = mem[in.apb_read_paddr];
             end
           end
