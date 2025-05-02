63f2bdfa8247e45f89718e07273cdec0bb742e4f
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-05-02 21:55:09 +0530

    Update apb_test.sv

--- src/testbench/apb_test.sv
+++ src/testbench/apb_test.sv
@@ -2,7 +2,7 @@ class apb_test extends uvm_test;
   `uvm_component_utils(apb_test)
   
   apb_env env;
-  apb_write0 seq;
+  apb_sequence seq;
   
   function new(string name = "apb_test", uvm_component parent);
     super.new(name, parent);
@@ -11,6 +11,7 @@ class apb_test extends uvm_test;
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     env = apb_env::type_id::create("env", this);
+    seq = apb_sequence::type_id::create("seq", this);
   endfunction
  
   function void end_of_elaboration_phase(uvm_phase phase);
@@ -21,13 +22,23 @@ class apb_test extends uvm_test;
   task run_phase(uvm_phase phase);
     super.run_phase(phase);
     phase.raise_objection(this);
-    seq = apb_write0::type_id::create("seq");
     repeat(5) begin
     seq.start(env.a_agent.seqr);
     end
-    `uvm_info(get_type_name(), $sformatf("Inside apb_wite0"), UVM_LOW);
-    `uvm_info(get_type_name(), $sformatf("Done apb_write0"), UVM_LOW);
-    
     phase.drop_objection(this);
   endtask
 endclass
+
+class apb_write0_slave0 extends apb_test;
+
+  `uvm_component_utils(apb_write0_slave0)
+
+  apb_write0_slave0 write0_slave0;
+    
+  function new(string name = " apb_write0_slave0", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+  endfunction
