633d5109ab61cfb22169869645f880b7516cc4ec
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-05-22 10:00:49 +0530

    Update apb_test.sv

--- src/test/apb_test.sv
+++ src/test/apb_test.sv
@@ -50,6 +50,29 @@ class apb_write0_slave0 extends apb_test;
  endtask
 endclass
 
+class apb_write1_slave1 extends apb_test;
+
+  `uvm_component_utils(apb_write1_slave1)
+
+  apb_write1 write1_slave1;
+    
+  function new(string name = " apb_write1_slave1", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    write1_slave1 = apb_write1::type_id::create("write1_slave1",this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+   phase.raise_objection(this);
+   write1_slave1.start(env.a_agent.seqr);
+   phase.drop_objection(this);
+   phase.phase_done.set_drain_time(this, 30);
+ endtask
+endclass
+
 
 class apb_read0_slave0 extends apb_test;
 
@@ -69,6 +92,29 @@ class apb_read0_slave0 extends apb_test;
   task run_phase(uvm_phase phase);
    phase.raise_objection(this);
    read0_slave0.start(env.a_agent.seqr);
+   phase.drop_objection(this);
+   phase.phase_done.set_drain_time(this, 30);
+ endtask
+endclass
+
+class apb_read1_slave1 extends apb_test;
+
+  `uvm_component_utils(apb_read1_slave1)
+
+  apb_read1 read1_slave1;
+    
+  function new(string name = " apb_read1_slave1", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    read1_slave1 = apb_read1::type_id::create("read1_slave1",this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+   phase.raise_objection(this);
+   read1_slave1.start(env.a_agent.seqr);
    phase.drop_objection(this);
    phase.phase_done.set_drain_time(this, 30);
  endtask
@@ -98,4 +144,98 @@ class apb_write_read0_slave0 extends apb_test;
   endtask
 endclass
 
+class apb_write_read1_slave1 extends apb_test;
 
+  `uvm_component_utils(apb_write_read1_slave1)
+
+  apb_write_read1 write_read1_slave1;
+    
+  function new(string name = " apb_write_read1_slave1", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    write_read1_slave1 = apb_write_read0::type_id::create("write_read1_slave1",this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    phase.raise_objection(this);
+    write_read1_slave1.start(env.a_agent.seqr);
+    phase.drop_objection(this);
+    phase.phase_done.set_drain_time(this, 100);
+  endtask
+endclass
+
+
+class reg_test extends apb_test;
+  `uvm_component_utils(reg_test)
+  
+   apb_write0 pkt1;
+   apb_write1 pkt2;
+   apb_read0  pkt3;
+   apb_read1  pkt4;
+   apb_write_read0 pkt5;
+   apb_write_read1 pkt6;
+
+  function new(string name = "apb_write_read0_slave0",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+   super.build_phase(phase);
+  endfunction
+
+  virtual function void end_of_elaboration();
+     uvm_top.print_topology();
+  endfunction
+
+
+  task run_phase (uvm_phase phase);
+
+    write_read0_slave0 = apb_write_read0::create("write_read0_slave0");
+    pkt1 = apb_write0::type_id::create("pkt1");
+    pkt2 = apb_write1::type_id::create("pkt2");
+    pkt3 = apb_read0::type_id::create("pkt3");
+    pkt4 = apb_read1::type_id::create("pkt4");
+    pkt5 = apb_write_read0::type_id::create("pkt5");
+    pkt6 = apb_write_read1::type_id::create("pkt6");
+
+    phase.raise_objection (this);
+    repeat(5)begin
+    pkt1.start(env.a_agent.seqr); 
+    phase.drop_objection (this);
+    phase.phase_done.set_drain_time(this,100);
+    end
+
+  repeat(5)begin
+   pkt2.start(env.a_agent.seqr); 
+   phase.drop_objection (this);
+   phase.phase_done.set_drain_time(this,100);
+    end
+ repeat(5)begin
+   pkt3.start(env.a_agent.seqr); 
+   phase.drop_objection (this);
+   phase.phase_done.set_drain_time(this,100);
+    end
+
+repeat(5)begin
+    pkt4.start(env.a_agent.seqr); 
+   phase.drop_objection (this);
+   phase.phase_done.set_drain_time(this,100);
+    end
+
+ repeat(5)begin
+    pkt5.start(env.a_agent.seqr); 
+   phase.drop_objection (this);
+   phase.phase_done.set_drain_time(this,100);
+    end
+ repeat(5)begin
+    pkt6.start(env.a_agent.seqr); 
+   phase.drop_objection (this);
+   phase.phase_done.set_drain_time(this,100);
+    end
+  endtask
+
+endclass
