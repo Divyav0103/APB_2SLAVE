3f619b2dc80ed0dfc3728483d0c1a19027bf7314
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-05-26 11:06:21 +0530

    Update apb_interface.sv

--- src/test/apb_interface.sv
+++ src/test/apb_interface.sv
@@ -15,13 +15,13 @@ interface apb_if(input bit pclk,input bit presetn);
   logic read_write;
   logic [`DW-1:0]apb_read_data_out;
   
-  clocking drv_cb@(posedge pclk or negedge presetn);
+  clocking drv_cb@(posedge pclk);
     default input #0 output #0;
     output transfer, apb_write_paddr, apb_write_data, apb_read_paddr, read_write;
     input presetn;
   endclocking
   
-  clocking mon_cb@(posedge pclk or negedge presetn);
+  clocking mon_cb@(posedge pclk);
     default input #0 output #0;
     input transfer, apb_write_paddr, apb_write_data, apb_read_paddr, read_write, apb_read_data_out;
   endclocking
