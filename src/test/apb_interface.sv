276d4de275b77c1c7e32677df280418230d0e580
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-05-29 10:16:20 +0530

    Update apb_interface.sv

--- src/test/apb_interface.sv
+++ src/test/apb_interface.sv
@@ -34,10 +34,13 @@ interface apb_if(input bit pclk,input bit presetn);
   transfer |-> (read_write ? !$isunknown(apb_read_paddr) : !$isunknown(apb_write_paddr));
 endproperty
 
+    //TRANSFER CHECK//
 assert property(check_transfer)
        $display("TRANSFER CHECK: ASSERTION PASS");
   else $error("TRANSFER CHECK: ASSERTION FAIL");
 
+   //VALID WRITE ADDRESS//
+  
   property check_valid_write_addr;
   @(posedge pclk) disable iff (!presetn)
   (!read_write && transfer) |-> !$isunknown(apb_write_paddr);
@@ -47,6 +50,7 @@ assert property(check_valid_write_addr)
        $display("VALID WRITE ADDRESS: ASSERTION PASS");
   else $error("VALID WRITE ADDRESS: ASSERTION FAIL");
 
+ //VALID READ ADDRESS//
   
 property check_valid_read_addr;
   @(posedge pclk) 
@@ -57,6 +61,7 @@ assert property(check_valid_read_addr)
        $display("VALID READ ADDRESS: ASSERTION PASS");
   else $error("VALID READ ADDRESS ASSERTION FAIL");
 
+  //VALID WRITE DATA//
   
 property check_valid_write_data;
   @(posedge pclk) 
@@ -67,16 +72,26 @@ assert property(check_valid_write_data)
        $display("VALID WRITE DATA: ASSERTION PASS");
   else $error("VALID WRITE DATA: ASSERTION FAIL");
 
+//VAILD READ DATA//
   
 property check_valid_read_data;
   @(posedge pclk) 
-  (read_write && transfer) |-> !$isunknown(apb_read_data_out);
+  (read_write && transfer) |=> !$isunknown(apb_read_data_out);
 endproperty
 
 assert property(check_valid_read_data)
        $display("VALID READ DATA: ASSERTION PASS");
-  else $error("VALID READ DATA: ASSERTION FAIL");
+  else $error("VALID READ DATA: ASSERTION FAIL",$time);
 
+//RESET CHECK//
+  property check_resetn;
+  @(posedge pclk)
+    (!presetn) |=> (transfer == 0 && read_write == 0 && apb_write_paddr == 9'b0 && apb_read_paddr == 9'b0);
+endproperty
+
+assert property(check_resetn)
+       $display("RESET CHECK: ASSERTION PASS");
+  else $error("RESET CHECK: ASSERTION FAIL", $time);
 
 
 endinterface
